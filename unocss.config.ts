101a2b900980730b61552dfb717d4f00b64eadb8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-04-09 15:45:25 +0200

    feat: set up config files

--- unocss.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'unocss';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-
-export default defineConfig({
-	rules: [],
-	shortcuts: [{}],
-	transformers: [transformerVariantGroup()],
-	theme: {}
-});
